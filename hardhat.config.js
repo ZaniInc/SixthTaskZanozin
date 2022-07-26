fb47e7d683b35f93b0c7faf5b13a9c5fe6b2be3a
ZaniInc <stridestoreholding@gmail.com>
2022-07-26 14:19:51 +0300

    setUp task6

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-truffle5");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.8.9",
+  // networks:{
+  //   ganache : {
+  //     url: "http://127.0.0.1:8545",
+  //   }
+  // },
+  solidity: "0.8.7",
 };
