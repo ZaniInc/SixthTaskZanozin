7c67d0e2910e28bd05810cddb1dc74ff45d61af2
ZaniInc <stridestoreholding@gmail.com>
2022-08-03 02:50:32 +0300

    update

---
+++ scripts/upgradeToV3.js
@@ -0,0 +1,18 @@
+
+const hre = require("hardhat");
+const owner = "0xC4aA7812309C2e9CF04fb0fC2Ba2492b8DaB1A16";
+const vestingV3 = "0x8FD6FE26b9e0EEbee108A731A47477C7230dcE6a";
+
+async function main() {
+
+  const ProxyContract = await hre.ethers.getContractFactory("TransparentUpgradeableProxy");
+  const proxy = await ProxyContract.attach("0x4871F59ADa92D0c16474c3Adef495c9A49921Ca3");
+  await proxy.upgradeTo(vestingV3);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
